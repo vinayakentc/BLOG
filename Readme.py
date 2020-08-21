cb956ff94edf7af5d1999bacf6d6f914c1a43d93
vinayakentc <66103397+vinayakentc@users.noreply.github.com>
2020-08-21 11:07:47 +0530

    Create Readme.py

---
+++ Readme.py
@@ -0,0 +1,13 @@
+
+Blog Website Using Python and Django
+
+
+
+•Tool: Atom, Pycharm
+•Language: Python3 , HTML, CSS, Bootstrap
+•Database: Sqlite3
+•Webframe Work: Django 
+Description :
+• A personal blogging web site written using Python and  Django framework 
+• Users can login, logout, create profile
+• Verified author can post a blog, delete blog 
